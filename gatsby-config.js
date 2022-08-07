1524b9e523b6a8f5b71c6cdf76faf2ea83e77235
Muhammad Badrul <badrulalomtawsyat@gmail.com>
2022-08-08 05:22:48 +0600

    Muhammad Badrul

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Muhammad Badrul',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
+      'Muhammad Badrul is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
     siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@bchiang7',
--- src/components/icons/codepen.js
+++ src/components/icons/codepen.js
@@ -4,19 +4,15 @@ const IconCodepen = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     role="img"
-    viewBox="0 0 24 24"
+    viewBox="0 0 24 25"
     fill="none"
     stroke="currentColor"
     strokeWidth="2"
     strokeLinecap="round"
     strokeLinejoin="round"
     className="feather feather-codepen">
-    <title>CodePen</title>
-    <polygon points="12 2 22 8.5 22 15.5 12 22 2 15.5 2 8.5 12 2"></polygon>
-    <line x1="12" y1="22" x2="12" y2="15.5"></line>
-    <polyline points="22 8.5 12 15.5 2 8.5"></polyline>
-    <polyline points="2 15.5 12 8.5 22 15.5"></polyline>
-    <line x1="12" y1="2" x2="12" y2="8.5"></line>
+    <title>Codeforces</title>
+    <path d="M4.5 7.5C5.328 7.5 6 8.172 6 9v10.5c0 .828-.672 1.5-1.5 1.5h-3C.673 21 0 20.328 0 19.5V9c0-.828.673-1.5 1.5-1.5h3zm9-4.5c.828 0 1.5.672 1.5 1.5v15c0 .828-.672 1.5-1.5 1.5h-3c-.827 0-1.5-.672-1.5-1.5v-15c0-.828.673-1.5 1.5-1.5h3zm9 7.5c.828 0 1.5.672 1.5 1.5v7.5c0 .828-.672 1.5-1.5 1.5h-3c-.828 0-1.5-.672-1.5-1.5V12c0-.828.672-1.5 1.5-1.5h3z"></path>
   </svg>
 );
 
